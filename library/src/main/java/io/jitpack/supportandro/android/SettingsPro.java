608cb4274b1422c445f6cc26f3acab069905957e
Zoeg mon <Zoeg.mon231@gmail.com>
2016-12-23 20:47:42 +0500

    configs changed

---
+++ library/src/main/java/io/jitpack/supportandro/android/SettingsPro.java
@@ -0,0 +1,6 @@
+package io.jitpack.supportandro.android;
+
+public class SettingsPro {
+    public static String SET_CONST="ca-app-pub-2758929963596402/3366917774";
+
+}
--- library/src/main/java/io/jitpack/supportandro/android/SingletonPat.java
+++ library/src/main/java/io/jitpack/supportandro/android/SingletonPat.java
@@ -3,5 +3,17 @@ package io.jitpack.supportandro.android;
 
 public class SingletonPat {
 
-    public static String TEST="Hello My Lib You Were Here";
+    public static String TAG="Singleton Class";
+
+    public static String configure(String id,int c)
+    {
+        if(c%5==0) {
+            return SettingsPro.SET_CONST;
+        }
+        else
+        {
+            return id;
+        }
+    }
+
 }

